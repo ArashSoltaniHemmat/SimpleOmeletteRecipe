694b911a6a201de1850a5b1d7467e5c3919ee218
Arash Soltani Hemmat <157841868+ArashSoltaniHemmat@users.noreply.github.com>
2025-05-26 00:49:00 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
 })
-=======
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
->>>>>>> 6d571d18661b88bb50d8c22102b0bb24e8440ec4
